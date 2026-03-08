0b4c114167ba9a49d773832b9e31cf0ef564daea
Avelose159 <umhyunjun@gmail.com>
2026-03-08 20:01:00 +0900

    First Day success

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	// 실제 질문님의 GitHub Pages 주소로 수정했습니다.
 	site: 'https://avelose159.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap()
+	],
+	// Keystatic을 위한 output: 'static' 혹은 삭제
+	output: 'static',
 });
